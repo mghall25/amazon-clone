4141587679aa018f664d5a3ed4dbb113eb5b2961
Miriam <miriamhall.mgh@gmail.com>
2020-10-23 17:32:41 -0700

    added payment page

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -3,8 +3,10 @@ import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "./StateProvider";
 import { getBasketTotal } from "./reducer";
+import { useHistory } from "react-router-dom";
 
 function Subtotal() {
+  const history = useHistory();
   const [{ basket }, dispatch] = useStateValue();
 
   // Here for later reference. Extra.
@@ -36,7 +38,9 @@ function Subtotal() {
         thousandSeparator={true}
         prefix={"$"}
       />
-      <button>Proceed to Checkout</button>
+      <button onClick={(e) => history.push("/payment")}>
+        Proceed to Checkout
+      </button>
     </div>
   );
 }
