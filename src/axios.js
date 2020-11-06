ce91a4fd2a681d81e5e4184c7b40f003a7386cc7
Miriam <miriamhall.mgh@gmail.com>
2020-11-06 13:49:09 -0800

    prepares for deployment

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-1035b/us-central1/api" // the api (cloud function ) url
+  // the API (cloud function ) URL
+  baseURL: "https://us-central1-clone-1035b.cloudfunctions.net/api",
+  // API url for testing
+  // baseURL: "http://localhost:5001/clone-1035b/us-central1/api",
 });
 
 export default instance;
